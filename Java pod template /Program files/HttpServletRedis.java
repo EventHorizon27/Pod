6d8da35f6f3391cf39a8a74f1a2fe7455e468a36
Cole Christini <cole.christini@gmail.com>
2017-09-23 15:59:57 -0400

    Rebuilt almost everything

---
+++ Java pod template /Program files/HttpServletRedis.java
@@ -0,0 +1,23 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import redis.clients.jedis.Jedis;
+
+public class HttpServletRedis extends HttpServlet {
+    Jedis jedis = new Jedis();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String key = request.getParameter("key");
+        try {
+            String ip = jedis.get(key);
+        } catch (NullPointerException n) {
+            response.getWriter().write("<html><body>invalid key</body></html>");
+
+        }
+
+        response.getWriter().write("<html><body>ip</body></html>");
+    }
+}
